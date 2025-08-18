05fd7c22d82657f3a878f70afcdb456d39211e67
nicolas-Perz <nicolasperez4209@gmail.com>
2025-08-18 18:31:30 -0300

    Añadi los input dentro de template/index.html y su interaccion correspondiente dentro de de app.py

--- app/app/app.py
+++ app/app/app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+
+    usuario_dinero = 1000
+    if request.method == 'POST':
+        monto_ingresado = request.form.get('ingresar',type=int)
+        if monto_ingresado:
+            usuario_dinero += monto_ingresado
+        else:
+            monto_ingresado = request.form.get('retirar',type=int)
+            if monto_ingresado:
+                usuario_dinero -= monto_ingresado
+
+    cursos = ['python','java','c++']
+    data = {
+        'titulo':'Menu principal',
+        'saludo':'Bienvenido!',
+        'cursos':cursos,
+        'cursos_cant':len(cursos),
+        'usuario_dinero': usuario_dinero
+    }
+
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
