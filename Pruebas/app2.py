be43f75d606e366809e6fcaa189a704869c3e892
FacaturaDev <gallardojuanbautista010@gmail.com>
2025-08-19 11:32:37 -0300

    (PRUEBAS) Creacion de layouts
    Desarrolle el HTML dinámico para la aplicación con su respectivo css y js, todo implementando las "templates" de Jinja2 y aprendiendo su uso.

--- Pruebas/app2.py
+++ Pruebas/app2.py
@@ -4,7 +4,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #lo que vamos a poner en esta funcion, son variables con el contenido que podremos mostrar como plantilla gracias a Jinja2, empezsamos creando un arreglo a modo de lista
+    cursos = ['Python', 'JavaScript', 'Java', 'C++', 'Ruby']
+    #creamos un diccionario con el contenido que queremos mostrar en la plantilla
+    data = {
+        'titulo': 'Mis Cursos',
+        'bienvenida': 'Bienvenido a mi sitio web de cursos',
+        'cursos': cursos,
+        'numero_cursos': len(cursos) #obtenemos el numero de cursos con "len()" que devuelve la longitud de este arreglo 
+    }
+    return render_template('index.html', data = data)
+
+@app.route('/contacto/<nombre>')
+def contacto(nombre):
+    data = {
+        'titulo': 'Contacto',
+        'nombre': nombre,
+    }
+    return render_template('contacto.html', nombre=nombre)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
