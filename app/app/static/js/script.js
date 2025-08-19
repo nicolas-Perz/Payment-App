0484e1d38465dcef0a3a888b17f0fbc73d218356
nicolas-Perz <nicolasperez4209@gmail.com>
2025-08-19 13:36:41 -0300

    Creacion de la vista base de la app (creacion de app/static)

---
+++ app/app/static/js/script.js
@@ -0,0 +1,6 @@
+let btn_transferir = document.getElementById('btn-transferir');
+let form_transferir = document.getElementById('form-transferir');
+
+btn_transferir.addEventListener('click', ()=>{
+    form_transferir.classList.toggle('invisible');
+})
